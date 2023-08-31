d66cf86d9c8a383d7c73db2960edfc3b7ecedb88
SousMour <142849312+SousMour@users.noreply.github.com>
2023-08-30 22:21:37 -0300

    Add files via upload

---
+++ meia_noite.py
@@ -0,0 +1,5 @@
+h=int(input())
+m=int(input())
+s=int(input())
+relogio= h* 3600 + m*60 + s
+print(relogio)
---
+++ reforma.py
@@ -0,0 +1,9 @@
+h=float(input())
+c=float(input())
+l=float(input())
+area_piso= l * c
+comprimento= h * c * l
+paredes= (2*h*l)+(2*h*c)
+print(area_piso)
+print(comprimento)
+print(paredes)
